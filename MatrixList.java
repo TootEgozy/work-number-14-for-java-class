bec658fdf9e348649e2f4210f6f770bcbe5d1c2d
TootEgozy <tootegozy@gmail.com>
2025-02-02 00:18:31 +0200

    ex 1 with documentation

--- MatrixList.java
+++ MatrixList.java
@@ -13,26 +13,27 @@ public class MatrixList
         _m00 = null;
     }
     
-    private void connectUpDownPointers(IntNodeMat node, int hop) {
-        IntNodeMat upperNode = null;
-    }
-    
+    // a helper method which connects a node to the one directly above it in the matrix.
+    // this method uses a loop to go up the list. hop is the number of required iterations to get to the node exactly 
+    // above the given node. once the loop is over the nodes pointers are set to point up / down at each other.
+    // if there is no node above (we are in the first row of the matrix) the method does nothing.    
     private void setUpperPointer(IntNodeMat node, int hop, boolean reverse) {
         IntNodeMat tempNode = node;
         boolean isReversed = reverse;
-        // go up the list and look for the node above node using hop to set their up / down pointers at each other.
-        // if there is no node above (last row) do nothing.
+        
+        // this loop just goes up the list (hop) times, and changes nothing with the nodes. 
+        // with each iteration it sets tempNode to be the node it reached.
         for(int i = 0; i < hop; i++) {
             if(isReversed) {
                 if(tempNode.getPrevCol() != null) tempNode = tempNode.getPrevCol(); // move left until the end of the row.
                 else if(tempNode.getPrevRow() != null) {
-                    tempNode = tempNode.getPrevRow();
+                    tempNode = tempNode.getPrevRow(); // go up
                     isReversed = !isReversed;
                 } else return; // if there is no upper row exit the function.
             } else {
                 if(tempNode.getNextCol() != null) tempNode = tempNode.getNextCol(); // move left until the end of the row.
                 else if(tempNode.getPrevRow() != null) {
-                    tempNode = tempNode.getPrevRow();
+                    tempNode = tempNode.getPrevRow(); // go up
                     isReversed = !isReversed;
                 } else return; // if there is no upper row exit the function.                
             }
@@ -41,26 +42,25 @@ public class MatrixList
         // set their up / down pointers to each other.
         node.setPrevRow(tempNode);
         tempNode.setNextRow(node);
-        
-        // ^ this was the end, following is debug:
-        int nodeValue = node.getData();
-        int tempNodeValue = tempNode.getData();
-        System.out.println("setting up and down pointers from setUpperPointer: ");
-        System.out.println(nodeValue +" up to: "+tempNodeValue);
     }
     
-    
+    // a helper method which gets the tail of the list, and traverses it to the beginning, 
+    // setting the node pointers to each other to "fold" the list into a matrix.
+    // the method returns the head of the list.
     private IntNodeMat addPointersToList(IntNodeMat tail, int colCount, int matLength) {
+        // the direction we begin with.
         boolean reverse = true;
         if(tail.getPrevCol() == null) reverse = false;
         
+        // hop is the distance (in nodes) which we need to traverse on the linear list to get to the node directly above it.
         int hop = (colCount * 2) - 1;
         IntNodeMat node = tail;
         IntNodeMat temp = node;
-        int counter = 0; // safty for the while loop to avoid endless looping in case of a bug. 
+        int counter = 0; // safty to avoid endless looping in case of a bug. 
         
+        // this loop handles pointing following nodes to each other (which are already connected by 1 pointer).
+        // to connect the nodes which are above or below each other in the matrix, we call the method setUpperPointer.
         while(counter < matLength) {
-            System.out.println("node is: "+node.getData());
             if((reverse && node.getPrevCol() == null) || (!reverse && node.getNextCol() == null )) {// node is the last of the row
                 if(node.getPrevRow() == null) return node; // node is also the end of the list so return it.
                 // node is the last of the row and there's a row above it. 
@@ -71,8 +71,10 @@ public class MatrixList
                 reverse = !reverse;
                 hop = (colCount * 2) -1;
             } else { // continue on the same row
+                // set the pointers between the node and the one directly above it.
                 setUpperPointer(node, hop, reverse);
                 hop = hop - 2;
+                // set the node pointer to the next or previous column.
                 if(reverse) {
                     temp = node;
                     node = node.getPrevCol();
@@ -89,15 +91,25 @@ public class MatrixList
         return null;
     }
     
+    // a helper method to create a node matrix from 2D array.
+    // this method constructs a zigzag linked list, and the calles addPointersToList to add more pointers
+    // to the nodes and "fold" the list into a matrix.
+    // it returns the head node of the matrix.
     private IntNodeMat matrixToNode(int[][] m) {     
         IntNodeMat prevNode = null;
-        int prevRowCount = 1;
+        // int prevRowCount = 1; ///////////////////////////////////////////////////////////!!!! delete this
+        // we start with the direction left to right.
         boolean reverse = false;
         
+        // these variables might be redundent but I believe that they help to simplify the code and add readability, 
+        // therefore I chose to define them here.
         int rowCount = m[0].length;
         int colCount = m.length;
         
         for(int i = 0; i < rowCount; i++) {
+            // for each matrix row, go over it in the ascribed direction. for each value, create a new node.
+            // point it to the previous node that was created, using the column pointer (prev or next) 
+            // if we are in the middle of the row,or the prevRow pointer if we are on the last cell of the row.
             if(reverse) {
                 for(int j = colCount - 1; j >= 0; j--) {
                     IntNodeMat newNode = new IntNodeMat(m[j][i]); 
@@ -118,15 +130,17 @@ public class MatrixList
                     prevNode = newNode;
                 }
             }
-            
         }
-        System.out.println("Finished creating the list, now adding pointers:");
+        // pass the linked list that was created to addPointersToList, so more pointers are added
+        // and all of the nodes are linked to their adjacent nodes.
+        // we pass the list last node and get back the head.
         IntNodeMat head = addPointersToList(prevNode, colCount, rowCount * colCount);
         return head;
     }
 
     public MatrixList(int[][]mat) {
         _m00 = matrixToNode(mat);
+
     }
     
     public void phase1PointerTester() {
